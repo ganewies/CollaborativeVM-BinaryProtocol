44f7f9888130aa03eecfa700d816560bc56caf55
Befaci <145709984+befaci03@users.noreply.github.com>
2025-06-22 10:14:25 +0200

    sound audio pls :P
    Credits to: wize-logic (https://github.com/wize-logic/collab-vm-1.2-binary-protocol/commit/b65f9ad909a7f3146dd9ca428afde53e4c606ee1)

--- src/CollabVMProtocolMessage.ts
+++ src/CollabVMProtocolMessage.ts
@@ -3,9 +3,11 @@ import { CollabVMRectMessage } from "./CollabVMRectMessage.js";
 export interface CollabVMProtocolMessage {
     type: CollabVMProtocolMessageType;
     rect?: CollabVMRectMessage | undefined;
+    opusPacket?: Buffer | Unit8Array | undefined;
 }
 
 export enum CollabVMProtocolMessageType {
     // JPEG Dirty Rectangle
     rect = 0,
+    audioOpus = 1, // OPUS Audio Message Type
 }
