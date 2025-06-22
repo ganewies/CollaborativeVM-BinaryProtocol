ca636b22b5080483ed6ac7d33c26d2aa81fcb277
Befaci <145709984+befaci03@users.noreply.github.com>
2025-06-22 12:42:22 +0200

    Uint not Unit lol

--- src/CollabVMProtocolMessage.ts
+++ src/CollabVMProtocolMessage.ts
@@ -3,7 +3,7 @@ import { CollabVMRectMessage } from "./CollabVMRectMessage.js";
 export interface CollabVMProtocolMessage {
     type: CollabVMProtocolMessageType;
     rect?: CollabVMRectMessage | undefined;
-    opusPacket?: Buffer | Unit8Array | undefined;
+    opusPacket?: Buffer | Uint8Array | undefined;
 }
 
 export enum CollabVMProtocolMessageType {
